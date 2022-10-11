a56599aa3f0e579d300b6546e809a3ce50552014
ingriid-ayaala <ingriidayaala@gmail.com>
2022-10-11 08:53:17 +0200

    h

---
+++ figuraGeometrica/src/dominio/FiguraGeometrica.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author ingri
+ */
+public abstract class  FiguraGeometrica  {
+    private String tipoFigura;
+    
+    //crear un metodo dibujar 
+}
